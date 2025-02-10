7a7fe3eee2d1d33e82178e067f33624bfa466a17
priya-singh27 <priya.beingdev@gmail.com>
2025-02-10 22:33:36 +0530

    Changed folder name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./components/Login/Login"
+import Login from './components/Login/Login'
 import Search from "./components/Search/Search"
 import "./App.css"
 
